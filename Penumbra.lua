dce6ea4f1d980d83a2e96a57a36ebf0e133f9635
lord-ruby <ruby@crimsonfang.com>
2026-03-30 17:43:46 +0100

    add jukebox

--- Penumbra.lua
+++ Penumbra.lua
@@ -1,9 +1,124 @@
-Penumbra = {}
+Penumbra = SMODS.current_mod
 Penumbra.loc_keys = {}
 Penumbra.loc_names = {} --bad practice to use this, exists because of jsons
-Penumbra.config = SMODS.current_mod.config
+Penumbra.loc_authors = {}
+Penumbra.tags = {}
 SMODS.Atlas {key = "modicon",path = "icon.png",px = 34,py = 34,}:register()
 Penumbra.config.Enabled = Penumbra.config.Enabled or {}	
+assert(SMODS.load_file("Jukebox.lua"))()
+
+function Penumbra.manual_parse(text, args)
+    if not text then return end
+    if type(text) ~= "table" then text = {text} end
+    local args = args or {}
+    local dir = G.localization
+    if args.loc_dir then
+        for _,v in ipairs(args.loc_dir) do
+            dir[v] = dir[v] or {}
+            dir = dir[v]
+        end
+    else
+        dir = G.localization.misc.v_text_parsed
+    end
+    local key = args.loc_key or "SMODS_stylize_text"
+    local function deep_find(t, index)
+        if type(index) ~= "table" then index = {index} end
+        for _,idv_index in ipairs(index) do
+            if t[idv_index] then return true end
+            for i,v in pairs(t) do
+                if i == idv_index then return true end
+                if type(v) == "table" then
+                    return deep_find(v, idv_index)
+                end
+            end
+        end
+        return false
+    end
+    if deep_find(text, "control") and not args.refresh then
+        if not args.no_loc_save then dir = text end
+        return text
+    end
+
+    local a = {"text", "name", "unlock"}
+    if not args.no_loc_save then
+        local loc = dir
+        loc[key] = {}
+        if deep_find(text, a) then
+            for _,v in ipairs(a) do
+                text[v] = text[v] or {}
+                text[v.."_parsed"] = (args.refresh and {}) or text[v.."_parsed"] or {}
+            end
+            if text.text then
+                for _,v in ipairs(text.text) do
+                    if type(v) == "table" then
+                        text.text_parsed[#text.text_parsed+1] = {}
+                        for _, vv in ipairs(v) do
+                            text.text_parsed[#text.text_parsed][#text.text_parsed[#text.text_parsed]+1] = loc_parse_string(vv)
+                        end
+                    else
+                        text.text_parsed[#text.text_parsed+1] = loc_parse_string(v)
+                    end
+                end
+            end
+            if text.name then
+                for _,v in ipairs((type(text.name) == "string" and {text.name}) or text.name) do
+                    text.name_parsed[#text.name_parsed+1] = loc_parse_string(v)
+                end
+            end
+            if text.unlock then
+                for _,v in ipairs(text.unlock) do
+                    text.unlock_parsed[#text.unlock_parsed+1] = loc_parse_string(v)
+                end
+            end
+            loc[key] = text
+        else
+            for i,v in ipairs(text) do
+                loc[key][i] = loc_parse_string(v)
+            end
+        end
+
+        return loc[key]
+    else
+        local loc = {}
+        if deep_find(text, a) then
+            for _,v in ipairs(a) do
+                text[v] = text[v] or {}
+                text[v.."_parsed"] = (args.refresh and {}) or text[v.."_parsed"] or {}
+            end
+            if text.text then
+                for _,v in ipairs(text.text) do
+                    if type(v) == "table" then
+                        text.text_parsed[#text.text_parsed+1] = {}
+                        for _, vv in ipairs(v) do
+                            text.text_parsed[#text.text_parsed][#text.text_parsed[#text.text_parsed]+1] = loc_parse_string(vv)
+                        end
+                    else
+                        text.text_parsed[#text.text_parsed+1] = loc_parse_string(v)
+                    end
+                end
+            end
+            if text.name then
+                for _,v in ipairs((type(text.name) == "string" and {text.name}) or text.name) do
+                    text.name_parsed[#text.name_parsed+1] = loc_parse_string(v)
+                end
+            end
+            if text.unlock then
+                for _,v in ipairs(text.unlock) do
+                    text.unlock_parsed[#text.unlock_parsed+1] = loc_parse_string(v)
+                end
+            end
+            loc = text
+        else
+            for i,v in ipairs(text) do
+                loc[i] = loc_parse_string(v)
+            end
+        end
+
+        return loc
+    end
+end
+
+
 PenumbraConfigTab = function()
 	if not G.PENUMBRA_PAGE then G.PENUMBRA_PAGE = 1 end
 	pbra_nodes = {
@@ -20,6 +135,12 @@ PenumbraConfigTab = function()
 		ref_table = Penumbra.config,
 		ref_value = "shuffle",
 	})
+	pbra_nodes2[#pbra_nodes2 + 1] = create_toggle({
+		label = localize("k_display_track_info"),
+		active_colour = HEX("40c76d"),
+		ref_table = Penumbra.config,
+		ref_value = "info",
+	})
 	local real_buffer = {}
 	for i, v in ipairs(SMODS.Sound.obj_buffer) do
 		if SMODS.Sound.obj_table[v].select_music_track or SMODS.Sound.obj_table[v].replace then
@@ -27,9 +148,10 @@ PenumbraConfigTab = function()
 			if Penumbra.config.Enabled[v] == nil then Penumbra.config.Enabled[v] = true end
 		end
 	end
-	local page = (G.PENUMBRA_PAGE and G.PENUMBRA_PAGE * 6 or 6) - (6 - 1)
-	local max_pages = math.floor(#real_buffer/6)
-	if max_pages * 6 < #real_buffer then --idk why this is needed but it is
+	local per_page = 7
+	local page = (G.PENUMBRA_PAGE and G.PENUMBRA_PAGE * per_page or per_page) - (per_page - 1)
+	local max_pages = math.floor(#real_buffer/per_page)
+	if max_pages *per_page < #real_buffer then --idk why this is needed but it is
 		max_pages = max_pages + 1
 	end
 	local sound_options = {}
@@ -39,10 +161,14 @@ PenumbraConfigTab = function()
 			localize("k_page") .. " " .. tostring(i) .. "/" .. tostring(max_pages)
 		)
 	end	
-	for i = page, math.min(page + 6 - 1, #real_buffer) do
+	for i = page, math.min(page + per_page - 1, #real_buffer) do
 		local key = real_buffer[i]
+		local astr = ""
+		if Penumbra.get_artist(key, Penumbra.tags[key]) ~= "" then
+			astr = " - "..Penumbra.get_artist(key, Penumbra.tags[key])
+		end
 		table.insert(pbra_nodes, create_toggle({
-			label = localize("b_play").." "..(Penumbra.loc_names[key] or (Penumbra.loc_keys[key] and localize(Penumbra.loc_keys[key])) or key),
+			label = localize("b_play").." "..Penumbra.get_name(key, Penumbra.tags[key] or {})..astr,
 			active_colour = HEX("40c76d"),
 			ref_table = Penumbra.config.Enabled,
 			ref_value = key,
@@ -95,8 +221,12 @@ SMODS.Sound.get_current_music = function(self)
 	for _, v in ipairs(self.obj_buffer) do
 		local s = self.obj_table[v]
 		if type(s.select_music_track) == 'function' then
+			if Penumbra.config.Enabled[v] == nil then
+				Penumbra.config.Enabled[v] = true
+			end
 			local res = s:select_music_track()
 			if not Penumbra.config.Enabled[v] then res = nil end
+			if s.key == PBJukebox.ActuallyPlaying then res = math.huge end
 			if res then
 				if type(res) ~= 'number' then res = -1e300 end
 				if not tracks[res] then tracks[res] = {} end
@@ -170,4 +300,113 @@ function play_sound(sound_code, ...)
 		if G.SHUFFLE_COUNT < 1 then G.SHUFFLE_INDEX = nil end
 	end
 	play_ref(sound_code, ...)
+end
+
+function Penumbra.GCM(loc)
+	local key = G.video_soundtrack or
+	(G.STATE == G.STATES.SPLASH and '') or
+	SMODS.Sound:get_current_music() or
+	(G.booster_pack_sparkles and not G.booster_pack_sparkles.REMOVED and 'music2') or
+	(G.booster_pack_meteors and not G.booster_pack_meteors.REMOVED and 'music3') or
+	(G.booster_pack and not G.booster_pack.REMOVED and not disable_booster_music and 'music2') or
+	(G.shop and not G.shop.REMOVED and 'music4') or
+	(G.GAME.blind and G.GAME.blind.boss and 'music5') or 
+	('music1')
+	if loc then
+		return (Penumbra.loc_names[key] or (Penumbra.loc_keys[key] and localize(Penumbra.loc_keys[key])) or key)
+	end
+	return key
+end
+
+function Penumbra.register_track(info)
+	Penumbra.loc_names[info.key] = info.name
+	Penumbra.loc_keys[info.key] = info.loc_key
+	Penumbra.loc_authors[info.key] = info.author
+	if info.icon_path then
+		local path = SMODS.current_mod.path .. info.icon_path
+		local image_data = SMODS.NFS.newFileData(path)
+		local image = love.graphics.newImage(image_data)
+		G.ASSET_ATLAS[info.key] = {
+			path = "",
+			full_path = "",
+			image_data = img_data,
+			px = image:getWidth(),
+			py = image:getHeight(),
+			image = image,
+			type = "",
+			name = name
+		}
+	end
+	local tag = {}
+	if SMODS.Sounds[info.key] then
+		_, tag = pcall(PBJukebox.read_music_tags, SMODS.Sounds[info.key].full_path, SMODS.Sounds[info.key].key)
+	end
+	Penumbra.tags[info.key] = tag
+	G.localization.descriptions.pbra_jukebox = G.localization.descriptions.pbra_jukebox or {}
+	G.localization.descriptions.pbra_jukebox[info.key] = G.localization.descriptions.pbra_jukebox[info.key] or {}
+	if Penumbra.get_name(info.key, tag) ~= "ERROR" then
+	G.localization.descriptions.pbra_jukebox[info.key].name = G.localization.descriptions.pbra_jukebox[info.key].name or
+		{ Penumbra.get_name(info.key, tag), "{C:edition,s:0.6}" .. Penumbra.get_artist(info.key, tag) }
+	end
+	G.localization.descriptions.pbra_jukebox[info.key].text = G.localization.descriptions.pbra_jukebox[info.key].text or
+		SMODS.Sounds[info.key] and SMODS.Sounds[info.key].pbra_purpose or { "???" }
+
+	Penumbra.manual_parse(G.localization.descriptions.pbra_jukebox, {})
+
+	if Penumbra.config.Enabled[info.key] == nil then
+		Penumbra.config.Enabled[info.key] = true
+	end
+end
+
+function Penumbra.display_track()
+	Penumbra.ease_towards_zero = true
+	Penumbra.nowplaying_timer = 0
+	Penumbra.nowplaying_xoff = Penumbra.nowplaying_xoff or -1000
+end
+
+function Penumbra.get_artist(key, tag)
+	if not SMODS.Sounds[key] then	
+		return "LouisF"
+	end
+	return Penumbra.loc_authors[key] or tag.artist or "???"
+end
+function Penumbra.get_name(key, tag)
+	return (Penumbra.loc_names[key] or (Penumbra.loc_keys[key] and localize(Penumbra.loc_keys[key]))) or tag.title or key or ""
+end
+
+local update_ref = Game.update
+function Game:update(dt, ...)
+	update_ref(self, dt, ...)
+	if Penumbra.nowplaying_timer then	
+		if Penumbra.ease_towards_zero then
+			local p = 1-(1.95*dt)
+			Penumbra.nowplaying_xoff = Penumbra.nowplaying_xoff * p + 20 * (1-p)
+			Penumbra.nowplaying_timer = Penumbra.nowplaying_timer + dt
+		elseif Penumbra.nowplaying_timer > 3.5 and not Penumbra.ease_towards_zero then
+			local p = 1-(1.95*dt)
+			Penumbra.nowplaying_xoff = Penumbra.nowplaying_xoff * p - (1000 * (1-p))
+			if Penumbra.nowplaying_xoff <= -999 then
+				Penumbra.nowplaying_timer = nil
+			end
+		end
+		if Penumbra.nowplaying_timer and (Penumbra.nowplaying_timer > 3.5) then
+			Penumbra.ease_towards_zero = nil
+		end
+	end
+end
+
+Penumbra.register_track({key = "music1", loc_key = "k_main_theme", author = "LouisF"})
+Penumbra.register_track({key = "music2", loc_key = "k_arcana_theme", author = "LouisF"})
+Penumbra.register_track({key = "music3", loc_key = "k_celestial_theme", author = "LouisF"})
+Penumbra.register_track({key = "music4", loc_key = "k_shop_theme", author = "LouisF"})
+Penumbra.register_track({key = "music5", loc_key = "k_boss_theme", author = "LouisF"})
+
+Penumbra.extra_tabs = function()
+	return {
+		-- Jukebox
+		{
+			label = "Jukebox",
+			tab_definition_function = PBJukebox.MusicTab
+		},
+	}
 end
--- localization/en-us.lua
+++ localization/en-us.lua
@@ -2,7 +2,20 @@ return {
     misc = {
         dictionary = {
             b_play = "Play",
-            k_shuffle_same_prio = "Shuffle songs with the same priority"
+            k_shuffle_same_prio = "Shuffle songs with the same priority",
+            k_display_track_info = "Display info when a new track plays",
+
+            k_main_theme = "Main Theme",
+            k_arcana_theme = "Arcana Pack Theme",
+            k_boss_theme = "Boss Theme",
+            k_celestial_theme = "Celestial Pack Theme",
+            k_shop_theme = "Shop Theme",
+
+            pbra_jukebox_request = "Request Song",
+            pbra_jukebox_unrequest = "Stop Playback",
+            pbra_jukebox_shuffle = "Random Song",
+            pbra_jukebox_default_music_title = "*",
+            pbra_current_track = "CURRENT TRACK",
         }
     }
 }
